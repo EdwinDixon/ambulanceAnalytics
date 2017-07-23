1dd9ade856fa337ed219eb3eb02022e3c9b35100
Edwin Dixon <edwin@factweavers.com>
2017-07-23 14:46:32 +0530

    added cors and mapping for the other fields in entity

--- src/main/java/com/analytics/ambulance/controller/ambulanceController.java
+++ src/main/java/com/analytics/ambulance/controller/ambulanceController.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,7 +17,8 @@ import java.util.List;
  * Created by lenovo on 16/7/17.
  */
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/")
 public class ambulanceController {
     private Logger logger = LoggerFactory.getLogger(ambulanceController.class);
--- src/main/java/com/analytics/ambulance/entities/Ambulance.java
+++ src/main/java/com/analytics/ambulance/entities/Ambulance.java
@@ -2,6 +2,7 @@ package com.analytics.ambulance.entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 
 /**
@@ -11,12 +12,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Ambulance {
     @Id
     String id;
+    @Field("amb_id")
     String ambulanceId;
+    @Field("amb_number")
     String licensePlateNumber;
+    @Field("driver_name")
     String driverName;
+    @Field("driver_licence_id")
     String driverLicenseNumber;
+    @Field("ownership")
     String ownerShip;
+    @Field("ownership_address")
     String ownerShipAddress;
+    @Field("location")
     Double[] location;
 
     public String getId() {
